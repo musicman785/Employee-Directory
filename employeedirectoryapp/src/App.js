1a7868e271da93c7239096aba5dbf9b3d53cf51b
Chris Salgado <musicman785@gmail.com>
2020-08-09 12:53:15 -0700

    Initial folder structure

---
+++ employeedirectoryapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am the Best React developer!</h1>
+    </div>
+  );
+}
+
+export default App;
