4eee048a20f33eea3f0f8d8f6f4658ac953b3e71
Chris Salgado <musicman785@gmail.com>
2020-08-09 13:47:31 -0700

    Header file created and added bootstrap

---
+++ employeedirectoryapp/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css"
+
+function Header() {
+    return (
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Employee Tracker</h1>
+                <p class="lead">View All Our Employees</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
